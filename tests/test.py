504a89f17f7e725b55af53979e3b26a8517077f5
reo <ramazanemreosmanoglu@gmail.com>
2024-03-24 22:53:14 +0300

    Add tests

---
+++ tests/test.py
@@ -0,0 +1,26 @@
+import unittest
+from observableprops import ObservablesMeta
+
+
+class ColorBox(metaclass=ObservablesMeta):
+    observable_properties = {
+        "color": "add_color_observer",
+    }
+
+
+class TestObservableProps(unittest.TestCase):
+    def test_color_change(self):
+        self.color_updated = False
+
+        def gui_update_color(new):
+            self.color_updated = True
+
+
+        colorbox = ColorBox()
+        colorbox.add_color_observer(gui_update_color)
+        colorbox.color = "Blue"  # All of the observers will be called
+
+        self.assertTrue(self.color_updated, "Color was not updated")
+
+if __name__ == '__main__':
+    unittest.main()
