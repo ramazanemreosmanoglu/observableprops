9a4d55dddef7f1f809aabab7a475f83e20dbdaa2
reo <ramazanemreosmanoglu@gmail.com>
2024-03-24 22:53:28 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="observableprops",
+    version="0.1.1",
+    description="Utility for creating properties and adding observers to them.",
+    author="reo6",
+    author_email="ramazanemreosmanoglu@gmail.com",
+    url="https://github.com/reo6/observableprops",
+    packages=find_packages(),
+    classifiers=[
+        "Topic :: Utilities",
+    ],
+    python_requires='>=3.10',
+)
