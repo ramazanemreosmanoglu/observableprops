c186e6173ac12cbfeae363bb6d6bfd02a8c18023
reo <ramazanemreosmanoglu@gmail.com>
2024-03-25 22:32:15 +0300

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="observableprops",
-    version="0.1.1",
+    version="0.2.0",
     description="Utility for creating properties and adding observers to them.",
     author="reo6",
     author_email="ramazanemreosmanoglu@gmail.com",
